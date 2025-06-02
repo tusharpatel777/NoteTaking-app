80fd15f128001f28383ca682d4c9c0b39efbe9d1
tusharpatel777 <tussipatel826@gmail.com>
2025-06-02 13:19:57 +0530

    prepared for deployement

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,14 +4,20 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors"
+import path from "path"
 
 
 dotenv.config();
 const app=express();
+const PORT=5000;
+const __dirname=path.resolve();
 
+
+if(process.env.NODE_ENV!=="production"){
 app.use(cors({
     origin:"http://localhost:5173"
 }));
+}
 
 app.use(rateLimiter);
 app.use(express.json());
@@ -19,9 +25,19 @@ app.use(express.json());
 
 
 app.use("/api/notes",notesRoutes);
+
+
+if(process.env.NODE_ENV==="production"){
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+app.get("*",(req,res)=>{
+
+    res.sendFile(path.join(__dirname,"../frontend","dis","index.html"))
+
+});
+}
   // it will ensure that the number req should less inorder to handle the large number 
 // console.log(v
-const PORT=5000;
+
 
 app.get("/",(req,res)=>{
     res.send("dekh chal ra hai ki ni");
