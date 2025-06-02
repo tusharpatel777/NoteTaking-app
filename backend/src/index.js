18d08fd7b8df5cce1b3ee7a55454be957c68b903
tusharpatel777 <tussipatel826@gmail.com>
2025-06-02 14:23:25 +0530

    fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -100,7 +100,7 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 // middleware
